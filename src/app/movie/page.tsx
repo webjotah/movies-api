74e48dbaaaebc401d92e947171b70cb49435a7e1
webjotah <nascimento.joao@aluno.ifsp.edu.br>
2023-08-16 10:58:05 -0300

    Getting all informations about a movie

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -1,30 +1,53 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
+import Youtube from "react-youtube";
+
+const opts = {
+  height: '320',
+  width: '570',
+}
 
 const Movie: React.FC = () => {
 
   const [storedTitle, setStoredTitle] = useState<any>(null);
+  const [trailer, setTrailer] = useState<any>(null);
+  const [date, setDate] = useState<any>(null);
+  const [overview, setOverview] = useState<any>(null);
+  const [poster, setPoster] = useState<any>(null);
+  const [runtime, setRuntime] = useState<any>(null);
+  const [votes, setVotes] = useState<any>(null);
+  const[genres, setGenres] = useState<any>(null);
   
 
   useEffect(() => {
-
     const handleStorageChange = () => {
       setTimeout(() => {
-        const newTitle = localStorage.getItem('titulo');
+        const newTitle = localStorage.getItem('title');
+        const newTrailer = localStorage.getItem('trailer');
+        const newDate = localStorage.getItem('release_date')?.split('-').reverse().join('/');
+        const newOverview = localStorage.getItem('overview');
+        const newPoster = localStorage.getItem('poster_path');
+        const newRuntime = localStorage.getItem('runtime');
+        const newVotes = localStorage.getItem('vote_average');
+
+        setVotes(newVotes);
+        setRuntime(newRuntime);
+        setPoster(newPoster);
+        setOverview(newOverview);
+        setDate(newDate);
+        setTrailer(newTrailer);
         setStoredTitle(newTitle);
       }, 500);
     };
 
-
     handleStorageChange();
   }, []);
-
-  console.log(storedTitle);
 
   return (
     <div>
       <h2>Título do Filme: {storedTitle}</h2>
+      <Youtube videoId={trailer} opts={opts}/>
     </div>
   );
 };
--- src/request/MovieRequest.tsx
+++ src/request/MovieRequest.tsx
@@ -11,10 +11,31 @@ const MovieSelection: React.FC = () =>  {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieSelected}?&api_key=c20492d18a33e7f71968eb5377feb1cb`)
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieSelected}?&api_key=c20492d18a33e7f71968eb5377feb1cb&append_to_response=videos,credits`)
         const data = await response.json();
-        localStorage.setItem('titulo', data.title);
+        console.log(data);
+        localStorage.setItem('title', data.title);
+        localStorage.setItem('poster', data.poster_path);
+        localStorage.setItem('overview', data.overview);
+        localStorage.setItem('release_date', data.release_date);
+        localStorage.setItem('runtime', data.runtime);
+        localStorage.setItem('vote_average', data.vote_average);
+        localStorage.setItem('genres', JSON.stringify(data.genres));
+        
+        const cast = data.credits.cast.map((actor: any) => ({
+          id: actor.id,
+          name: actor.name,
+          character: actor.character,
+          profile_path: actor.profile_path,
+        }));
+        localStorage.setItem('cast', JSON.stringify(cast));
 
+        for(let i = 0; i < data.videos.results.length; i++){
+          if(data.videos.results[i].type === "Trailer"){
+            localStorage.setItem('trailer', data.videos.results[i].key);
+            break;
+          }
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
